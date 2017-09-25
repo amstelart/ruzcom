56b096ae2b9a41d2bb3f720324e007d012087bf7
Артем <amstelart@MacBook-Pro-Artem.local>
2017-09-25 18:19:04 +0300

    dev-05

---
+++ src/blocks/contacts/contacts.js
@@ -0,0 +1,30 @@
+ymaps.ready(function() {
+	var myMap_01 = new ymaps.Map('contacts__map_01', {
+		center: [55.589285, 37.911447],
+		zoom: 16
+  });
+  var myMap_02 = new ymaps.Map('contacts__map_02', {
+    center: [55.589447, 37.912840],
+    zoom: 16
+  });
+
+	myMap_01.geoObjects
+		.add(new ymaps.Placemark([55.589285, 37.911447], {
+			balloonContent: 'цвет <strong>голубой</strong>',
+			iconCaption: '6-й микрорайон, с4А'
+		}, {
+			preset: 'islands#redCircleDotIconWithCaption',
+			iconCaptionMaxWidth: '130'
+		}));
+
+
+	myMap_02.geoObjects
+		.add(new ymaps.Placemark([55.589447, 37.912840], {
+			balloonContent: 'цвет <strong>голубой</strong>',
+			iconCaption: 'улица Степана Степанова, 9Г'
+		}, {
+			preset: 'islands#redCircleDotIconWithCaption',
+			iconCaptionMaxWidth: '200'
+		}));
+
+});
